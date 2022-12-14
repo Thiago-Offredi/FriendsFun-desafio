b6911fba62ce33ef39070ba9a879325aa2b1484a
Thiago-Offredi <114181435+Thiago-Offredi@users.noreply.github.com>
2022-12-14 11:34:42 -0300

    Add files via upload
    Formulario

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+ <FormCadastro />
+ <CadastroFunComponent />
+</template>
+
+<script>
+import CadastroFunComponent from './components/CadastroFun-component.vue';
+import  FormCadastro from './components/FormCadastro.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormCadastro,
+    CadastroFunComponent
+}
+}
+
+</script>
+
+<style>
+body{
+  background-color: rgb(255, 255, 255);
+}
+*{
+  margin:0;
+  padding:0;
+}
+</style>
